e336f60da794c0788ac7b37c59e7929228dc090f
Alex Sanchez-Stern <alex.sanchezstern@gmail.com>
2023-02-20 12:22:04 -0800

    Set up abstracting backend with backwards compatibility

---
+++ src/coq_serapy/coq_agent.py
@@ -0,0 +1,399 @@
+#!/usr/bin/env python3
+
+import re
+
+from typing import Optional, List, Union, Iterable, Tuple
+from dataclasses import dataclass
+
+from .util import eprint, unwrap
+from .coq_backend import CoqBackend
+from .coq_util import (kill_comments, preprocess_command,
+                       possibly_starting_proof, ending_proof,
+                       lemmas_defined_by_stmt, update_sm_stack,
+                       setup_opam_env)
+from .contexts import TacticContext, ProofContext
+
+@dataclass
+class FileState:
+    in_proof: bool
+    tactic_history: Optional['TacticHistory']
+
+    local_lemmas: List[Tuple[str, bool]]
+    local_lemmas_cache: Optional[List[str]]
+    sm_stack: List[Tuple[str, bool]]
+    module_changed: bool
+    def __init__(self) -> None:
+        self.in_proof = False
+        self.tactic_history = None
+        self.local_lemmas = []
+        self.local_lemmas_cache = None
+        self.sm_stack = []
+        self.module_changed = True
+
+    @property
+    def module_stack(self) -> List[str]:
+        return [entry for entry, is_section in self.sm_stack
+                if not is_section]
+
+    @property
+    def section_stack(self) -> List[str]:
+        return [entry for entry, is_section in self.sm_stack
+                if is_section]
+    @property
+    def sm_prefix(self) -> str:
+        return "".join([sm + "." for sm, is_sec in self.sm_stack])
+
+    @property
+    def module_prefix(self) -> str:
+        return "".join([module + "." for module in self.module_stack])
+
+    def add_potential_local_lemmas(self, cmd: str) -> None:
+        lemmas = lemmas_defined_by_stmt(self.module_prefix, cmd)
+        is_section = "Let" in cmd
+        for lemma in lemmas:
+            self.local_lemmas.append((lemma, is_section))
+            if lemma.startswith(self.module_prefix):
+                cached = lemma[len(self.module_prefix):].replace('\n', '')
+            else:
+                cached = lemma.replace("\n", "")
+            if self.local_lemmas_cache is not None:
+                self.local_lemmas_cache.append(cached)
+
+    def cancel_potential_local_lemmas(self, cmd: str) -> None:
+        lemmas = lemmas_defined_by_stmt(self.module_prefix, cmd)
+        is_section = "Let" in cmd
+        for lemma in lemmas:
+            self.local_lemmas.remove((lemma, is_section))
+
+    def remove_potential_local_lemmas(self, cmd: str) -> None:
+        reset_match = re.match(r"Reset\s+(.*)\.", cmd)
+        if reset_match:
+            reseted_lemma_name = self.module_prefix + reset_match.group(1)
+            for (lemma, is_section) in list(self.local_lemmas):
+                if lemma == ":":
+                    continue
+                lemma_match = re.match(r"\s*([\w'\.]+)\s*:", lemma)
+                assert lemma_match, f"{lemma} doesnt match!"
+                lemma_name = lemma_match.group(1)
+                if lemma_name == reseted_lemma_name:
+                    self.local_lemmas.remove((lemma, is_section))
+        abort_match = re.match(r"\s*Abort", cmd)
+        if abort_match:
+            self.local_lemmas.pop()
+
+    def add_potential_smstack_cmd(self, cmd: str) -> None:
+        new_stack = update_sm_stack(self.sm_stack, cmd)
+        if len(self.sm_stack) > 0 and \
+           self.sm_stack[-1][1] and \
+           len(new_stack) < len(self.sm_stack):
+            self.local_lemmas = \
+                [(lemma, is_section) for (lemma, is_section)
+                 in self.local_lemmas if not is_section]
+        if len(new_stack) != len(self.sm_stack):
+            self.module_changed = True
+        self.sm_stack = new_stack
+
+class CoqAgent:
+    backend: CoqBackend
+    _file_state: FileState
+    verbosity: int
+
+    def __init__(self, backend: CoqBackend,
+                 root_dir: Optional[str] = None,
+                 verbosity: int = 0) -> None:
+        self.backend = backend
+        self._file_state = FileState()
+        self.verbosity = verbosity
+        self.root_dir = root_dir
+        if root_dir:
+            self._exec_includes(root_dir)
+        self.run_stmt("Unset Printing Notations.")
+
+    def __enter__(self) -> 'CoqAgent':
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+        self.backend.close()
+
+    def run_stmt(self, stmt: str, timeout: Optional[int] = None) -> None:
+        # We need to escape some stuff so that it doesn't get stripped
+        # too early.
+        stmt = stmt.replace("\\", "\\\\")
+        stmt = stmt.replace("\"", "\\\"")
+        # Kill the comments early so we can recognize comments earlier
+        stmt = kill_comments(stmt)
+
+        for stm in preprocess_command(stmt):
+            if not self._file_state.in_proof:
+                self._file_state.add_potential_smstack_cmd(stm)
+                self._file_state.add_potential_local_lemmas(stm)
+                if possibly_starting_proof(stm) and self.backend.isInProof():
+                    self._file_state.tactic_history = TacticHistory()
+                    self._file_state.in_proof = True
+
+            else:
+                if ending_proof(stm):
+                    self._file_state.in_proof = False
+                    self._file_state.tactic_history = None
+                self._file_state.remove_potential_local_lemmas(stm)
+            # Track goal opening/closing
+            is_goal_open = re.match(r"\s*(?:\d+\s*:)?\s*[{]\s*", stm)
+            is_goal_close = re.match(r"\s*[}]\s*", stm)
+            if self._file_state.in_proof:
+                assert self._file_state.tactic_history
+                if is_goal_open:
+                    self._file_state.tactic_history.openSubgoal()
+                elif is_goal_close:
+                    self._file_state.tactic_history.closeSubgoal()
+                else:
+                    self._file_state.tactic_history.addTactic(stm)
+
+            self.backend.addStmt(stm, timeout=timeout)
+
+    def cancel_last(self) -> None:
+        if self._file_state.in_proof:
+            assert self._file_state.tactic_history
+            cancelled = self._file_state.tactic_history.getNextCancelled()
+            self._file_state.cancel_potential_local_lemmas(cancelled)
+            eprint(f"Cancelling command {cancelled}", guard=self.verbosity >= 2)
+            self._file_state.tactic_history.removeLast()
+        self.backend.cancelLastStmt(cancelled)
+        if self._file_state.in_proof and possibly_starting_proof(cancelled) and \
+           not self.backend.isInProof():
+            self._file_state.in_proof = False
+            self._file_state.tactic_history = None
+        elif not self._file_state.in_proof and ending_proof(cancelled):
+            self._file_state.in_proof = True
+            self._file_state.tactic_history = TacticHistory()
+
+    def run_into_next_proof(self, commands: List[str]) \
+            -> Tuple[List[str], List[str]]:
+        assert not self.backend.isInProof(), "We're already in a proof"
+        commands_iter = iter(commands)
+        commands_run = []
+        for command in commands_iter:
+            self.run_stmt(command, timeout=120)
+            commands_run.append(command)
+            if self.backend.isInProof():
+                return list(commands_iter), commands_run
+        return [], commands_run
+    def finish_proof(self, commands: List[str]) \
+            -> Optional[Tuple[List[str], List[str]]]:
+        assert self.backend.isInProof(), "We're already out of a proof"
+        commands_iter = iter(commands)
+        commands_run = []
+        for command in commands_iter:
+            self.run_stmt(command, timeout=60)
+            commands_run.append(command)
+            if not self.backend.isInProof():
+                return list(commands_iter), commands_run
+        return None
+    def reset(self) -> None:
+        self._file_state = FileState()
+        self.run_stmt("Reset Initial.")
+        if self.root_dir:
+            self._exec_includes(self.root_dir)
+        self.run_stmt("Unset Printing Notations.")
+    @property
+    def goals(self) -> str:
+        proof_context = self.backend.getProofContext()
+        if proof_context and proof_context.fg_goals:
+            return proof_context.fg_goals[0].goal
+        return ""
+
+    @property
+    def hypotheses(self) -> List[str]:
+        proof_context = self.backend.getProofContext()
+        if proof_context and proof_context.fg_goals:
+            return proof_context.fg_goals[0].hypotheses
+        return []
+
+    @property
+    def prev_tactics(self):
+        return self._file_state.tactic_history.getCurrentHistory()
+
+    @property
+    def local_lemmas(self) -> List[str]:
+        def generate() -> Iterable[str]:
+            for (lemma, _is_section) in self._file_state.local_lemmas:
+                if lemma.startswith(self._file_state.module_prefix):
+                    yield lemma[len(self._file_state.module_prefix):].replace('\n', '')
+                else:
+                    yield lemma.replace('\n', '')
+        if self._file_state.module_changed:
+            self._file_state.local_lemmas_cache = list(generate())
+            self._file_state.module_changed = False
+        return unwrap(self._file_state.local_lemmas_cache)[:-1]
+
+    @property
+    def cur_lemma(self) -> str:
+        return self.local_lemmas[-1]
+
+    @property
+    def cur_lemma_name(self) -> str:
+        match = re.match(r"\s*([\w'\.]+)\s+:.*", self.cur_lemma)
+        assert match, f"Can't match {self.cur_lemma}"
+        return match.group(1)
+
+    @property
+    def proof_context(self) -> Optional[ProofContext]:
+        return self.backend.getProofContext()
+
+    def tactic_context(self, relevant_lemmas) -> TacticContext:
+        return TacticContext(relevant_lemmas,
+                             self.prev_tactics,
+                             self.hypotheses,
+                             self.goals)
+
+    def check_term(self, term: str) -> str:
+        return self.backend.queryVernac(f"Check {term}.")[0]
+    def locate_ident(self, ident: str) -> str:
+        return "\n".join(self.backend.queryVernac(f"Locate {ident}."))
+    def interrupt(self) -> None:
+        self.backend.interrupt()
+    def get_lemmas_about_head(self) -> List[str]:
+        proof_context = self.proof_context
+        assert proof_context, "Can't run get_lemmas_about_head when not in a proof!"
+        head = proof_context.focused_goal.split()[0]
+        print("head is", head)
+        return self.search_about(head)
+    def search_about(self, symbol: str) -> List[str]:
+        return self.backend.queryVernac(f"Search {symbol}.")
+
+    def _exec_includes(self, root_dir: str) -> None:
+        try:
+            with open(root_dir + "/_CoqProject", 'r') as includesfile:
+                includes_string = includesfile.read()
+        except FileNotFoundError:
+            try:
+                with open(root_dir + "/Make", 'r') as includesfile:
+                    includes_string = includesfile.read()
+            except FileNotFoundError:
+                eprint(f"Didn't find _CoqProject or Make for {root_dir}")
+                includes_string = ""
+
+        for rmatch in re.finditer(r"-R\s*(\S*)\s*(\S*)\s*", includes_string):
+            self.run_stmt(
+                f"Add Rec LoadPath \"{rmatch.group(1)}\" as {rmatch.group(2)}.")
+        for qmatch in re.finditer(r"-Q\s*(\S*)\s*(\S*)\s*", includes_string):
+            self.run_stmt(
+                f"Add LoadPath \"{qmatch.group(1)}\" as {qmatch.group(2)}.")
+        for imatch in re.finditer(r"-I\s*(\S*)", includes_string):
+            self.run_stmt(
+                f"Add ML Path \"{imatch.group(1)}\".")
+
+
+
+@dataclass
+class TacticTree:
+    children: List[Union['TacticTree', str]]
+    isClosed: bool
+
+    def __repr__(self) -> str:
+        result = "["
+        for child in self.children:
+            result += repr(child)
+            result += ","
+        result += "]"
+        return result
+
+
+class TacticHistory:
+    __tree: TacticTree
+    __cur_subgoal_depth: int
+
+    def __init__(self) -> None:
+        self.__tree = TacticTree([], False)
+        self.__cur_subgoal_depth = 0
+
+    def openSubgoal(self) -> None:
+        curTree = self.__tree
+        for _ in range(self.__cur_subgoal_depth):
+            assert isinstance(curTree.children[-1], TacticTree)
+            curTree = curTree.children[-1]
+        curTree.children.append(TacticTree([], False))
+        self.__cur_subgoal_depth += 1
+
+
+    def closeSubgoal(self) -> None:
+        curTree = self.__tree
+        for _ in range(self.__cur_subgoal_depth):
+            assert isinstance(curTree.children[-1], TacticTree)
+            curTree = curTree.children[-1]
+        curTree.isClosed = True
+        assert self.__cur_subgoal_depth > 0
+        self.__cur_subgoal_depth -= 1
+
+    def curDepth(self) -> int:
+        return self.__cur_subgoal_depth
+
+    def addTactic(self, tactic: str) -> None:
+        curTree = self.__tree
+        for _ in range(self.__cur_subgoal_depth):
+            assert isinstance(curTree.children[-1], TacticTree)
+            curTree = curTree.children[-1]
+        curTree.children.append(tactic)
+
+    def removeLast(self) -> None:
+        assert len(self.__tree.children) > 0, \
+            "Tried to remove from an empty tactic history!"
+        curTree = self.__tree
+        for _ in range(self.__cur_subgoal_depth):
+            assert isinstance(curTree.children[-1], TacticTree)
+            curTree = curTree.children[-1]
+        if len(curTree.children) == 0:
+            parent = self.__tree
+            for _ in range(self.__cur_subgoal_depth-1):
+                assert isinstance(parent.children[-1], TacticTree)
+                parent = parent.children[-1]
+            parent.children.pop()
+            self.__cur_subgoal_depth -= 1
+        else:
+            lastChild = curTree.children[-1]
+            if isinstance(lastChild, str):
+                curTree.children.pop()
+            else:
+                assert isinstance(lastChild, TacticTree)
+                self.__cur_subgoal_depth += 1
+                lastChild.isClosed = False
+
+    def getCurrentHistory(self) -> List[str]:
+        def generate() -> Iterable[str]:
+            curTree = self.__tree
+            for i in range(self.__cur_subgoal_depth+1):
+                yield from (child for child in curTree.children
+                            if isinstance(child, str))
+                if i < self.__cur_subgoal_depth:
+                    assert isinstance(curTree.children[-1], TacticTree)
+                    curTree = curTree.children[-1]
+        return list(generate())
+
+    def getFullHistory(self) -> List[str]:
+        def generate(tree: TacticTree) -> Iterable[str]:
+            for child in tree.children:
+                if isinstance(child, TacticTree):
+                    yield "{"
+                    yield from generate(child)
+                    if child.isClosed:
+                        yield "}"
+                else:
+                    yield child
+        return list(generate(self.__tree))
+
+    def getNextCancelled(self) -> str:
+        curTree = self.__tree
+        assert len(curTree.children) > 0, \
+            "Tried to cancel from an empty history"
+        for _ in range(self.__cur_subgoal_depth):
+            assert isinstance(curTree.children[-1], TacticTree)
+            curTree = curTree.children[-1]
+
+        if len(curTree.children) == 0:
+            return "{"
+        if isinstance(curTree.children[-1], TacticTree):
+            return "}"
+        assert isinstance(curTree.children[-1], str), curTree.children[-1]
+        return curTree.children[-1]
+
+    def __str__(self) -> str:
+        return f"depth {self.__cur_subgoal_depth}, {repr(self.__tree)}"
--- src/coq_serapy/lsp_backend.py
+++ src/coq_serapy/lsp_backend.py
@@ -7,11 +7,13 @@ import os
 import queue
 import functools
 
-from typing import Any, Dict, List, cast, Callable
+from typing import Any, Dict, List, cast, Callable, Optional
 
 import pylspclient
 
 from .contexts import ProofContext, Obligation
+from .coq_backend import CoqBackend
+from .coq_util import setup_opam_env
 
 class QueuePipe(threading.Thread):
 
@@ -28,8 +30,7 @@ class QueuePipe(threading.Thread):
     def get(self) -> str:
         return self.queue.get()
 
-class CoqLSPyInstance:
-    server_version: str
+class CoqLSPyInstance(CoqBackend):
     proc: Any
     stderr_queue: QueuePipe
     messageQueues: Dict[str, queue.Queue]
@@ -40,17 +41,18 @@ class CoqLSPyInstance:
     doc_version: int
     doc_sentences: List[str]
     state_dirty: bool
-    cached_context: ProofContext
+    cached_context: Optional[ProofContext]
 
 
-    def __init__(self, lsp_command: str, server_version: str) -> None:
+    def __init__(self, lsp_command: str,
+                 root_dir: Optional[str] = None,
+                 timeout: int = 30) -> None:
         setup_opam_env()
         self.proc = subprocess.Popen(lsp_command, stdin=subprocess.PIPE,
                                      stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                                      shell=True)
         self.stderr_queue = QueuePipe(self.proc.stderr)
         self.stderr_queue.start()
-        self.server_version = server_version
 
         queuedMessages = ['window/logMessage', '$/logTrace']
         printedMessages: List[str] = []
@@ -66,12 +68,13 @@ class CoqLSPyInstance:
                                                                   msgqueue))
                                  for msg_type, msgqueue in self.messageQueues.items()},
                               **{msg_type: functools.partial(print, msg_type) for msg_type in printedMessages},
-                              **{msg_type: lambda x: None for msg_type in ignoredMessages}})
+                              **{msg_type: lambda x: None for msg_type in ignoredMessages}},
+            timeout=timeout)
         self.lsp_client = pylspclient.LspClient(self.endpoint)
-        root_uri = '.'
+        root_uri = root_dir or '.'
         workspace_folders = [{'name': 'coq-lsp', 'uri': root_uri}]
         capabilities: Dict[str, Any] = {}
-        self.lsp_client.initialize(self.proc.pid, '.', root_uri, None,
+        self.lsp_client.initialize(self.proc.pid, root_dir or '.', root_uri, None,
                                    capabilities,
                                    "off", workspace_folders)
         self.verify_init_messages()
@@ -82,10 +85,9 @@ class CoqLSPyInstance:
         self.doc_sentences = []
         self._openEmptyDoc()
 
-    def _openEmptyDoc(self) -> None:
-        docContents = "\n".join(self.doc_sentences)
-        self.open_doc = "local1.v"
-
+    def openDoc(self, filename: str) -> None:
+        self.open_doc = filename
+        docContents = ""
 
         self.doc_version = 1
         self.lsp_client.didOpen({"uri": self.open_doc,
@@ -98,14 +100,20 @@ class CoqLSPyInstance:
             r'\[check\]: resuming(?: \[v: \d+\])?, from: 0 l: \d+',
             r'\[check\]: done \[\d+\.\d+\]: document fully checked .*',
             r'\[cache\]: hashing: \d+.\d+ | parsing: \d+.\d+ | exec: \d+.\d+',
-            r'\[cache\]: .*'] # v0.01
+            r'\[cache\]: .*']
         for msg in msgs:
             self.checkMessagePattern('$/logTrace', msg)
+
+    def _openEmptyDoc(self) -> None:
+        self.openDoc("local1.v")
 
     def __enter__(self) -> 'CoqLSPyInstance':
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+        self.close()
+
+    def close(self) -> None:
         self.lsp_client.shutdown()
         self.lsp_client.exit()
         self.proc.terminate()
@@ -126,39 +134,27 @@ class CoqLSPyInstance:
             f"Message {message['message']} doesn't match pattern {message_pattern}"
 
     def verify_init_messages(self) -> None:
-        if self.server_version == "0.01":
-            self.checkMessage('window/logMessage', "Server started") # v0.01
-            self.checkInMessage('window/logMessage', "Configuration loaded") # v0.01
-            expected_msgs = ['[process_queue]: Serving Request: initialize',
-                             '[init]: custom client options:',
-                             '[init]: [init]: {}',
-                             '[version]: any',
-                             '[init]: client capabilities:',
-                             '[init]: [init]: {}'] # v0.01
-        else:
-            assert self.server_version == "0.1.4"
+        self.checkMessage('window/logMessage', "Initializing server") # v0.1.4
+        self.checkMessage('window/logMessage', "Server initialized") # v0.1.4
 
-            self.checkMessage('window/logMessage', "Initializing server") # v0.1.4
-            self.checkMessage('window/logMessage', "Server initialized") # v0.1.4
-
-            self.checkInMessage('window/logMessage', "Configuration loaded") # v0.1.4
-            expected_msgs = ['[init]: custom client options:',
-                             '[init]: [init]: {}',
-                             '[client_version]: any',
-                             '[workspace]: initialized'] # v0.1.4
+        self.checkInMessage('window/logMessage', "Configuration loaded") # v0.1.4
+        expected_msgs = ['[init]: custom client options:',
+                         '[init]: [init]: {}',
+                         '[client_version]: any',
+                         '[workspace]: initialized'] # v0.1.4
 
         for expected_msg in expected_msgs:
             self.checkMessage("$/logTrace", expected_msg)
 
-    def addStmt(self, stmt: str) -> None:
+    def addStmt(self, stmt: str, timeout:Optional[int] = None) -> None:
         self.doc_sentences.append(stmt.rstrip("\n"))
         self.state_dirty = True
 
-    def cancelLastStmt(self) -> None:
+    def cancelLastStmt(self, cancelled: str) -> None:
         self.doc_sentences.pop()
         self.state_dirty = True
 
-    def getProofContext(self) -> ProofContext:
+    def getProofContext(self) -> Optional[ProofContext]:
         if not self.state_dirty:
             return self.cached_context
 
@@ -168,56 +164,49 @@ class CoqLSPyInstance:
             {"uri": self.open_doc,
              "version": self.doc_version},
             [{"text": doc}])
+        line = len(self.doc_sentences)
+        character = len(self.doc_sentences[-1]) if line > 0 else 0
         response = self.endpoint.call_method(
             "proof/goals", textDocument={"uri": self.open_doc},
-            position={"line": len(self.doc_sentences),
-                      "character": len(self.doc_sentences[-1])})
-        self.cached_context = self.parseGoalResponse(response)
+            position={"line": line,
+                      "character": character})
+        self.cached_context = parseGoalResponse(response)
         self.state_dirty = False
         return self.cached_context
 
-    def parseObligation(self, obl_obj: Dict[str, Any]) -> Obligation:
-        return Obligation([
-            ", ".join(hyp_obj["names"]) + " : " + hyp_obj["ty"]
-            for hyp_obj in obl_obj["hyps"]],
-                          obl_obj["ty"])
+    def isInProof(self) -> bool:
+        return self.getProofContext() is not None
 
-    def parseGoalResponse(self, response: Dict[str, Any]) -> ProofContext:
-        goals = response["goals"]
-        return ProofContext([self.parseObligation(obl_obj)
-                             for obl_obj in goals["goals"]],
-                            [self.parseObligation(obl_obj)
-                             for stack in goals["stack"]
-                             for substack in stack
-                             for obl_obj in substack],
-                            [self.parseObligation(obl_obj)
-                             for obl_obj in goals["shelf"]],
-                            [self.parseObligation(obl_obj)
-                             for obl_obj in goals["given_up"]])
+    def queryVernac(self, vernac: str) -> List[str]:
+        raise NotImplementedError()
+    def interrupt(self) -> None:
+        raise NotImplementedError()
 
-def setup_opam_env() -> None:
-    env_string = subprocess.run(f"opam env", shell=True, stdout=subprocess.PIPE,
-                                text=True, check=True).stdout
-    _setup_opam_env_from_str(env_string)
+def parseObligation(obl_obj: Dict[str, Any]) -> Obligation:
+    return Obligation([
+        ", ".join(hyp_obj["names"]) + " : " + hyp_obj["ty"]
+        for hyp_obj in obl_obj["hyps"]],
+                      obl_obj["ty"])
 
-
-def _setup_opam_env_from_str(env_string: str) -> None:
-    for env_line in env_string.splitlines():
-        linematch = re.fullmatch(r"(\w*)='([^;]*)'; export (\w*);", env_line)
-        assert linematch, env_line
-        envvar = linematch.group(1)
-        assert envvar == linematch.group(3)
-        envval = linematch.group(2)
-        os.environ[envvar] = envval
-
-def logTrace(message_queue, params):
-    message_queue.put(params)
-    pass
+def parseGoalResponse(response: Dict[str, Any]) -> Optional[ProofContext]:
+    goals = response["goals"]
+    if goals is None:
+        return None
+    return ProofContext([parseObligation(obl_obj)
+                         for obl_obj in goals["goals"]],
+                        [parseObligation(obl_obj)
+                         for stack in goals["stack"]
+                         for substack in stack
+                         for obl_obj in substack],
+                        [parseObligation(obl_obj)
+                         for obl_obj in goals["shelf"]],
+                        [parseObligation(obl_obj)
+                         for obl_obj in goals["given_up"]])
 
 def main():
 
-    with CoqLSPyInstance("cd $HOME/research/coq-lsp && dune exec -- coq-lsp",
-                         "0.1.4") as coq:
+    with CoqLSPyInstance("cd $HOME/research/coq-lsp && dune exec -- coq-lsp") as coq:
+        print(coq.getProofContext())
         coq.addStmt("Theorem nat_refl : forall n : nat, n= n.")
         coq.addStmt("Proof.")
         coq.addStmt("intro.")
