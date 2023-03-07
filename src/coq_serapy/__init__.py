4697f25cfc48f76794a7829dee067fdf095a5c4b
Alex Sanchez-Stern <sanchezstern@cs.umass.edu>
2023-03-07 16:50:02 -0500

    Set verbosity properly in CoqContext api

--- src/coq_serapy/__init__.py
+++ src/coq_serapy/__init__.py
@@ -80,7 +80,7 @@ def CoqContext(prelude: str = ".", verbosity: int = 0, set_env: bool = True) \
     try:
         if minor_version < 16:
             backend = CoqSeraPyInstance(["sertop", "--implicit"], root_dir=prelude,
-                                        set_env=set_env)
+                                        set_env=set_env, verbosity=verbosity)
         else:
             backend = CoqLSPyInstance("coq-lsp", root_dir=prelude, set_env=set_env)
         agent = CoqAgent(backend, prelude, verbosity=verbosity)
