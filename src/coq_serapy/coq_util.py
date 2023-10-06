6a588699b2a6989081b3b0f80d930c091432d83e
Alex Sanchez-Stern <alex.sanchezstern@gmail.com>
2023-10-06 18:44:18 +0000

    Allow timing qeds without crashing

--- src/coq_serapy/coq_util.py
+++ src/coq_serapy/coq_util.py
@@ -651,7 +651,7 @@ def possibly_starting_proof(command: str) -> bool:
 
 def ending_proof(command: str) -> bool:
     stripped_command = kill_comments(command).strip()
-    return (re.match(r"Qed\s*\.", stripped_command) or
+    return (re.match(r"(?:Time\s+)?Qed\s*\.", stripped_command) or
             re.match(r"Defined\s*(?:\S*)?\.", stripped_command) or
             re.match(r"Admitted\s*\.", stripped_command) or
             stripped_command == "Abort." or
